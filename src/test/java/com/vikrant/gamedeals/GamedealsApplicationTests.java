226f9478f41f0aa7565951c2535b4bd2c1195476
vikrantSangwan08 <vickyvirus101@gmail.com>
2025-10-02 13:55:02 +0530

    intial commit

---
+++ src/test/java/com/vikrant/gamedeals/GamedealsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vikrant.gamedeals;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamedealsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
